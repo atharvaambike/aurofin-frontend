30c249701f44a751a133a987382c2b2bdf7ea7ed
atharvaambike <atharvaambike@gmail.com>
2025-10-19 01:06:32 +0530

    added list of loans and apply loan

--- src/app/modules/customer/listofloans/listofloans.ts
+++ src/app/modules/customer/listofloans/listofloans.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoanSchemeDto } from '../../../entity/LoanSchemeDto';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listofloans',
@@ -6,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './listofloans.html',
   styleUrl: './listofloans.css'
 })
-export class Listofloans {
+export class Listofloans implements OnInit {
+  loanSchemes: LoanSchemeDto[] = [];
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadLoanSchemes();
+  }
+
+  loadLoanSchemes() {
+    this.http.get<LoanSchemeDto[]>('http://localhost:8080/loan-app/loans').subscribe({
+      next: (res) => this.loanSchemes = res,
+      error: (err) => console.error('Error fetching loan schemes:', err)
+    });
+  }
+
+  applyLoan(scheme: LoanSchemeDto) {
+    this.router.navigate(['/customer/applyloan'], {
+      queryParams: { loanId: scheme.loanTypeId }
+    });
+  }
 }
